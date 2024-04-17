f7f322a2e5ae1cf72177defd79625d8967c7c35f
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2024-04-17 12:04:36 +0200

    feat: add more benchmarks

--- packages/rusty_chacha/rust/benches/example.rs
+++ packages/rusty_chacha/rust/benches/example.rs
@@ -2,6 +2,7 @@ use divan::{counter::BytesCount, Bencher};
 use embedded_rusty_chacha::api::{
     decrypt, decrypt_from_file, encrypt, encrypt_to_file, generate_cha_cha_20_key,
 };
+use rand::{thread_rng, Rng};
 use sysinfo::System;
 
 fn main() {
@@ -33,8 +34,8 @@ fn main() {
     divan::main();
 }
 
-// different sizes from 1 MB to 200 MB
-#[divan::bench(args = [1, 10, 100, 200], max_time = 2)]
+// different sizes from 1 MB to 180 MB
+#[divan::bench(args = [1, 10, 100, 180], max_time = 2)]
 fn bench_encrypt(bencher: Bencher, size: usize) {
     let size = size * 1024 * 1024; // MB to bytes
 
@@ -46,8 +47,8 @@ fn bench_encrypt(bencher: Bencher, size: usize) {
         });
 }
 
-// different sizes from 1 MB to 200 MB
-#[divan::bench(args = [1, 10, 100, 200], max_time = 2)]
+// different sizes from 1 MB to 180 MB
+#[divan::bench(args = [1, 10, 100, 180], max_time = 2)]
 fn bench_decrypt(bencher: divan::Bencher, size: usize) {
     let size = size * 1024 * 1024; // MB  to bytes
     let key = generate_cha_cha_20_key();
@@ -65,8 +66,8 @@ fn bench_decrypt(bencher: divan::Bencher, size: usize) {
         });
 }
 
-// different sizes from 1 MB to 200 MB
-#[divan::bench(args = [1, 10, 100, 200], max_time = 2)]
+// different sizes from 1 MB to 180 MB
+#[divan::bench(args = [1, 10, 100, 180], max_time = 2)]
 fn bench_encrypt_to_file(bencher: Bencher, size: usize) {
     let size = size * 1024 * 1024; // MB to bytes
 
@@ -79,8 +80,8 @@ fn bench_encrypt_to_file(bencher: Bencher, size: usize) {
         });
 }
 
-// different sizes from 1 MB to 200 MB
-#[divan::bench(args = [1, 10, 100, 200], max_time = 2)]
+// different sizes from 1 MB to 180 MB
+#[divan::bench(args = [1, 10, 100, 180], max_time = 2)]
 fn bench_decrypt_from_file(bencher: Bencher, size: usize) {
     let size = size * 1024 * 1024; // MB to bytes
     let key = generate_cha_cha_20_key();
@@ -103,4 +104,67 @@ fn bench_decrypt_from_file(bencher: Bencher, size: usize) {
         .bench_values(|(key, _)| {
             let _ = decrypt_from_file(key, "test_file.bin".to_string(), None).unwrap();
         });
+}
+
+#[divan::bench(args = [
+    (0.0, -1000), (0.0, 3), (0.0, 7), (0.0, 12),
+    (0.25, -1000), (0.25, 3), (0.25, 7), (0.25, 12),
+    (0.5, -1000), (0.5, 3), (0.5, 7), (0.5, 12),
+],
+max_time = 3)]
+fn bench_compress_encrypt(bencher: Bencher, args: (f64, i32)) {
+    let size = 180 * 1024 * 1024; // 800 MB
+
+    let (data_randomness, compression_level) = args;
+
+    bencher
+        .counter(BytesCount::new(size))
+        .with_inputs(|| {
+            (
+                generate_cha_cha_20_key(),
+                generate_test_data(size, data_randomness),
+            )
+        })
+        .bench_values(|(key, cleartext)| {
+            let _ = encrypt(key, cleartext, None, Some(compression_level)).unwrap();
+        });
+}
+
+#[divan::bench(args = [
+    (0.0, -1000), (0.0, 3), (0.0, 7), (0.0, 12),
+    (0.25, -1000), (0.25, 3), (0.25, 7), (0.25, 12),
+    (0.5, -1000), (0.5, 3), (0.5, 7), (0.5, 12),
+],
+max_time = 3)]
+fn bench_decrypt_decompress(bencher: Bencher, args: (f64, i32)) {
+    let size = 180 * 1024 * 1024; // 180 MB
+
+    let key = generate_cha_cha_20_key();
+    let (data_randomness, compression_level) = args;
+
+    bencher
+        .counter(BytesCount::new(size))
+        .with_inputs(|| {
+            (key.clone(), {
+                let cleartext = generate_test_data(size, data_randomness);
+                encrypt(key.clone(), cleartext, None, Some(compression_level)).unwrap()
+            })
+        })
+        .bench_values(|(key, ciphertext)| {
+            let _ = decrypt(key, ciphertext, None).unwrap();
+        });
+}
+
+fn generate_test_data(size: usize, randomness: f64) -> Vec<u8> {
+    let mut rng = thread_rng();
+    let data: Vec<u8> = (0..size)
+        .map(|_| {
+            if rng.gen_bool(randomness) {
+                rng.gen()
+            } else {
+                0
+            }
+        })
+        .collect();
+    data
 }
