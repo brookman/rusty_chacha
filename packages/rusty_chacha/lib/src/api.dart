3d6c974d3ae9c7b217db16810776b4c01a6e8128
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2024-04-25 12:35:10 +0200

    feat: update docs

--- packages/rusty_chacha/lib/src/api.dart
+++ packages/rusty_chacha/lib/src/api.dart
@@ -167,7 +167,7 @@ class RustyXChaCha20Poly1305 {
   static Future<Uint8List> generateKey({dynamic hint}) =>
       RustLib.instance.api.rustyXChaCha20Poly1305GenerateKey(hint: hint);
 
-  /// Important: A nonce must only be used once!
+  /// Important: A nonce must only be used once.
   /// Do not encrypt multiple pieces of data with the same nonce.
   static Future<Uint8List> generateNonce({dynamic hint}) =>
       RustLib.instance.api.rustyXChaCha20Poly1305GenerateNonce(hint: hint);
