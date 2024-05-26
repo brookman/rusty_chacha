56aa712cebeb2982636b7f81f5b25df0452cc374
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2024-05-26 15:38:08 +0200

    chore: upgrade dependency

--- packages/rusty_chacha/macos/Classes/frb.h
+++ packages/rusty_chacha/macos/Classes/frb.h
@@ -47,77 +47,78 @@ typedef struct wire_cst_rusty_x_cha_cha_20_poly_1305 {
   struct wire_cst_compression compression;
 } wire_cst_rusty_x_cha_cha_20_poly_1305;
 
-void frbgen_rusty_chacha_wire_compress(int64_t port_,
-                                       struct wire_cst_list_prim_u_8_loose *data,
-                                       int32_t zstd_compression_level);
+void frbgen_rusty_chacha_wire__crate__api__compress(int64_t port_,
+                                                    struct wire_cst_list_prim_u_8_loose *data,
+                                                    int32_t zstd_compression_level);
 
-void frbgen_rusty_chacha_wire_decompress(int64_t port_, struct wire_cst_list_prim_u_8_loose *data);
+void frbgen_rusty_chacha_wire__crate__api__decompress(int64_t port_,
+                                                      struct wire_cst_list_prim_u_8_loose *data);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_create_internal(int64_t port_,
-                                                                         struct wire_cst_list_prim_u_8_strict *key,
-                                                                         struct wire_cst_compression *compression);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_create_internal(int64_t port_,
+                                                                                      struct wire_cst_list_prim_u_8_strict *key,
+                                                                                      struct wire_cst_compression *compression);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_decrypt(int64_t port_,
-                                                                 struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
-                                                                 struct wire_cst_list_prim_u_8_loose *ciphertext,
-                                                                 struct wire_cst_list_prim_u_8_strict *aad);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_decrypt(int64_t port_,
+                                                                              struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
+                                                                              struct wire_cst_list_prim_u_8_loose *ciphertext,
+                                                                              struct wire_cst_list_prim_u_8_strict *aad);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_decrypt_from_file(int64_t port_,
-                                                                           struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
-                                                                           struct wire_cst_list_prim_u_8_strict *file_path,
-                                                                           struct wire_cst_list_prim_u_8_strict *aad,
-                                                                           uint64_t *offset);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_decrypt_from_file(int64_t port_,
+                                                                                        struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
+                                                                                        struct wire_cst_list_prim_u_8_strict *file_path,
+                                                                                        struct wire_cst_list_prim_u_8_strict *aad,
+                                                                                        uint64_t *offset);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_encrypt(int64_t port_,
-                                                                 struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
-                                                                 struct wire_cst_list_prim_u_8_loose *cleartext,
-                                                                 struct wire_cst_list_prim_u_8_strict *nonce,
-                                                                 struct wire_cst_list_prim_u_8_strict *aad);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_encrypt(int64_t port_,
+                                                                              struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
+                                                                              struct wire_cst_list_prim_u_8_loose *cleartext,
+                                                                              struct wire_cst_list_prim_u_8_strict *nonce,
+                                                                              struct wire_cst_list_prim_u_8_strict *aad);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_encrypt_to_file(int64_t port_,
-                                                                         struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
-                                                                         struct wire_cst_list_prim_u_8_loose *cleartext,
-                                                                         struct wire_cst_list_prim_u_8_strict *file_path,
-                                                                         struct wire_cst_list_prim_u_8_strict *nonce,
-                                                                         struct wire_cst_list_prim_u_8_strict *aad,
-                                                                         bool *append);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_encrypt_to_file(int64_t port_,
+                                                                                      struct wire_cst_rusty_cha_cha_20_poly_1305 *that,
+                                                                                      struct wire_cst_list_prim_u_8_loose *cleartext,
+                                                                                      struct wire_cst_list_prim_u_8_strict *file_path,
+                                                                                      struct wire_cst_list_prim_u_8_strict *nonce,
+                                                                                      struct wire_cst_list_prim_u_8_strict *aad,
+                                                                                      bool *append);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_generate_key(int64_t port_);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_generate_key(int64_t port_);
 
-void frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_generate_nonce(int64_t port_);
+void frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_generate_nonce(int64_t port_);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_create_internal(int64_t port_,
-                                                                           struct wire_cst_list_prim_u_8_strict *key,
-                                                                           struct wire_cst_compression *compression);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_create_internal(int64_t port_,
+                                                                                        struct wire_cst_list_prim_u_8_strict *key,
+                                                                                        struct wire_cst_compression *compression);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_decrypt(int64_t port_,
-                                                                   struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
-                                                                   struct wire_cst_list_prim_u_8_loose *ciphertext,
-                                                                   struct wire_cst_list_prim_u_8_strict *aad);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_decrypt(int64_t port_,
+                                                                                struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
+                                                                                struct wire_cst_list_prim_u_8_loose *ciphertext,
+                                                                                struct wire_cst_list_prim_u_8_strict *aad);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_decrypt_from_file(int64_t port_,
-                                                                             struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
-                                                                             struct wire_cst_list_prim_u_8_strict *file_path,
-                                                                             struct wire_cst_list_prim_u_8_strict *aad,
-                                                                             uint64_t *offset);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_decrypt_from_file(int64_t port_,
+                                                                                          struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
+                                                                                          struct wire_cst_list_prim_u_8_strict *file_path,
+                                                                                          struct wire_cst_list_prim_u_8_strict *aad,
+                                                                                          uint64_t *offset);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_encrypt(int64_t port_,
-                                                                   struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
-                                                                   struct wire_cst_list_prim_u_8_loose *cleartext,
-                                                                   struct wire_cst_list_prim_u_8_strict *nonce,
-                                                                   struct wire_cst_list_prim_u_8_strict *aad);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_encrypt(int64_t port_,
+                                                                                struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
+                                                                                struct wire_cst_list_prim_u_8_loose *cleartext,
+                                                                                struct wire_cst_list_prim_u_8_strict *nonce,
+                                                                                struct wire_cst_list_prim_u_8_strict *aad);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_encrypt_to_file(int64_t port_,
-                                                                           struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
-                                                                           struct wire_cst_list_prim_u_8_loose *cleartext,
-                                                                           struct wire_cst_list_prim_u_8_strict *file_path,
-                                                                           struct wire_cst_list_prim_u_8_strict *nonce,
-                                                                           struct wire_cst_list_prim_u_8_strict *aad,
-                                                                           bool *append);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_encrypt_to_file(int64_t port_,
+                                                                                        struct wire_cst_rusty_x_cha_cha_20_poly_1305 *that,
+                                                                                        struct wire_cst_list_prim_u_8_loose *cleartext,
+                                                                                        struct wire_cst_list_prim_u_8_strict *file_path,
+                                                                                        struct wire_cst_list_prim_u_8_strict *nonce,
+                                                                                        struct wire_cst_list_prim_u_8_strict *aad,
+                                                                                        bool *append);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_generate_key(int64_t port_);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_generate_key(int64_t port_);
 
-void frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_generate_nonce(int64_t port_);
+void frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_generate_nonce(int64_t port_);
 
 bool *frbgen_rusty_chacha_cst_new_box_autoadd_bool(bool value);
 
@@ -144,22 +145,22 @@ static int64_t dummy_method_to_enforce_bundling(void) {
     dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_cst_new_box_autoadd_u_64);
     dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_cst_new_list_prim_u_8_loose);
     dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_cst_new_list_prim_u_8_strict);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_compress);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_decompress);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_create_internal);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_decrypt);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_decrypt_from_file);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_encrypt);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_encrypt_to_file);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_generate_key);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_cha_cha_20_poly_1305_generate_nonce);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_create_internal);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_decrypt);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_decrypt_from_file);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_encrypt);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_encrypt_to_file);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_generate_key);
-    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire_rusty_x_cha_cha_20_poly_1305_generate_nonce);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__compress);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__decompress);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_create_internal);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_decrypt);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_decrypt_from_file);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_encrypt);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_encrypt_to_file);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_generate_key);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_cha_cha_20_poly_1305_generate_nonce);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_create_internal);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_decrypt);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_decrypt_from_file);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_encrypt);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_encrypt_to_file);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_generate_key);
+    dummy_var ^= ((int64_t) (void*) frbgen_rusty_chacha_wire__crate__api__rusty_x_cha_cha_20_poly_1305_generate_nonce);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
     return dummy_var;
 }
