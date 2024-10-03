4c42506b99e948a4d96d2e9bd1365def859ec18e
Benjamin Bachmann <beba@Benjamins-MacBook-Pro.local>
2024-10-03 16:16:19 +0200

    fix: remove unused code

--- packages/rusty_chacha_dart/rust/build.rs
+++ packages/rusty_chacha_dart/rust/build.rs
@@ -35,15 +35,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-
-/// Find the location of the `target/` directory. Note that this may be 
-/// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR` 
-/// variable.
-fn target_dir() -> PathBuf {
-    if let std::result::Result::Ok(target) = env::var("CARGO_TARGET_DIR") {
-        PathBuf::from(target)
-    } else {
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
-    }
-}
